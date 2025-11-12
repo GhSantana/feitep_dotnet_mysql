1979f76b5c2cb75a0172484dc43bf9b26e59888c
GhSantana <gustavossantana960@gmail.com>
2025-11-11 22:13:38 -0300

    Inicio

---
+++ Feitep.MySql.Models/Cliente.cs
@@ -0,0 +1,10 @@
+﻿namespace Feitep.MySql.Models;
+
+public class Cliente
+{
+    public int id { get; set; }
+
+    public string? Nome { get; set; }
+
+public DateTime DataNascimento { get; set; }
+}
---
+++ Feitep.MySql.Repository/SistemaContext.cs
@@ -0,0 +1,8 @@
+﻿namespace Feitep.MySql.Repository;
+
+using Feitep.MySql.Models;
+using Microsoft.EntityFrameworkCore;
+public class SistemaContext : DbContext
+{
+public DbSet<Cliente>? Clientes { get; set; }
+}
